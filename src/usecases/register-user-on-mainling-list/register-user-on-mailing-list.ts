0918b7b6cbc468121c9687e547a0071287c1c613
Brendo Katagi <brendo.katagi@gmail.com>
2021-01-27 16:03:59 -0300

    refactor: use path mapping to improve imports

--- src/usecases/register-user-on-mainling-list/register-user-on-mailing-list.ts
+++ src/usecases/register-user-on-mainling-list/register-user-on-mailing-list.ts
@@ -1,7 +1,7 @@
-import { InvalidEmailError, InvalidNameError } from '../../entities/errors'
-import { User, UserData } from '../../entities'
-import { Either, left, right } from '../../shared'
-import { UserRepository } from './ports'
+import { InvalidEmailError, InvalidNameError } from '@/entities/errors'
+import { User, UserData } from '@/entities'
+import { Either, left, right } from '@/shared'
+import { UserRepository } from '@/usecases/register-user-on-mainling-list/ports'
 
 export class RegisterUserOnMainlingList {
   private readonly userRepo: UserRepository
--- test/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
+++ test/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -1,7 +1,7 @@
-import { UserData } from '../../../src/entities'
-import { UserRepository } from '../../../src/usecases/register-user-on-mainling-list/ports'
-import { RegisterUserOnMainlingList } from '../../../src/usecases/register-user-on-mainling-list'
-import { InMemoryUserRepository } from './repository'
+import { UserData } from '@/entities'
+import { UserRepository } from '@/usecases/register-user-on-mainling-list/ports'
+import { RegisterUserOnMainlingList } from '@/usecases/register-user-on-mainling-list'
+import { InMemoryUserRepository } from '@test/usecases/register-user-on-mailing-list/repository'
 
 describe('Register user on mainling list use case', () => {
   test('should add  user with complete data to mailing list', async () => {
