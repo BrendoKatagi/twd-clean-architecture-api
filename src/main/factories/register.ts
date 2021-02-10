ad8688ba8e990b4586f68bc49f6523ec355c1a10
Brendo Katagi <brendo.katagi@gmail.com>
2021-02-10 14:07:19 -0300

    feat: register user controller factory

---
+++ src/main/factories/register.ts
@@ -0,0 +1,10 @@
+import { RegisterUserController, ResgisterUserController } from '@/web-controllers'
+import { RegisterUserOnMainlingList } from '@/usecases/register-user-on-mainling-list/'
+import { InMemoryUserRepository } from '@test/usecases/register-user-on-mailing-list/repository'
+
+export const makeRegisterUserController = (): RegisterUserController => {
+  const inMemoryUserRepository = new InMemoryUserRepository([])
+  const registerUserOnMainlingListUseCase = new RegisterUserOnMainlingList(inMemoryUserRepository)
+  const registerUserController = new RegisterUserController(registerUserOnMainlingListUseCase)
+  return registerUserController
+}
