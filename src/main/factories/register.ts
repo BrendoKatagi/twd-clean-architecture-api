b4f925f884cd2a53eec2f0b0a494832a9394763e
Brendo Katagi <brendo.katagi@gmail.com>
2021-02-18 15:12:10 -0300

    refactor: move in memory repository to source folder

--- src/main/factories/register.ts
+++ src/main/factories/register.ts
@@ -1,6 +1,6 @@
 import { RegisterUserController } from '@/web-controllers'
 import { RegisterUserOnMainlingList } from '@/usecases/register-user-on-mainling-list/'
-import { InMemoryUserRepository } from '@test/usecases/register-user-on-mailing-list/repository'
+import { InMemoryUserRepository } from '@/usecases/register-user-on-mainling-list/repository'
 
 export const makeRegisterUserController = (): RegisterUserController => {
   const inMemoryUserRepository = new InMemoryUserRepository([])
